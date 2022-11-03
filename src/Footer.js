71515dea93d2a631897819b62c1b1b7660b3f3c4
sachin <sachin.bhawsar150@gmail.com>
2022-11-03 17:25:22 +0530

    added react3

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+
+const Footer=()=>
+
+{
+	return(
+		<>
+		<div className="mt-4 p-5 bg-dark text-white rounded">
+		 <p>Cybrom Technology Pvt. Ltd.Bhopal Bhopal (MP)</p>
+		</div>
+  
+
+		</>
+
+
+		);
+}
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+
+
+const Header=()=>
+
+{
+	
+	return(
+		<>
+		<div class="container-fluid p-5 bg-primary text-white text-center">
+        <h1>welcome to header part</h1>
+        <p>Resize this responsive page to see the effect!</p> 
+        </div>
+		</>
+		);
+	
+}
+
+
+export default Header;
